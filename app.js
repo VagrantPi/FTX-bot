bf634e5df3670c3844ef6a301484bb1417775c6b
kais <xup6u06m4@gmail.com>
2021-07-16 15:20:00 +0800

    tmp

--- app.js
+++ app.js
@@ -4,23 +4,23 @@ const HttpAgent = require('./lib/HttpAgent');
 const main = async () => {
   const config = await Utils.readConfig()
   const httpAgent = new HttpAgent()
-  const timestamp = new Date().getTime().toString()
+  // const timestamp = new Date().getTime().toString()
 
-  const signature = Utils.sign({
-    apiMethod: 'GET',
-    apiPath: 'account', 
-    timestamp,
-    secret: config.ftx.apiSecret
-  })
-  console.log('signature', signature);
+  // const signature = Utils.sign({
+  //   apiMethod: 'GET',
+  //   apiPath: 'account', 
+  //   timestamp,
+  //   secret: config.ftx.apiSecret
+  // })
+  // console.log('signature', signature);
 
-  const res = await httpAgent.get({
-    'FTX-KEY': config.ftx.apiKey,
-    'FTX-SIGN': signature,
-    'FTX-TS': timestamp,
-    'Content-Type': 'application/json; utf-8',
-  })
-  console.log('res', res);
+  // const headers = {
+  //   'FTX-KEY': config.ftx.apiKey,
+  //   'FTX-SIGN': signature,
+  //   'FTX-TS': timestamp,
+  // }
+  const tmp = await httpAgent.get({ path: 'account', secret: config.ftx.apiSecret, apiKey: config.ftx.apiKey })
+  console.log('tmp', tmp);
 }
 
 main()
