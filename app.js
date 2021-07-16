045748d3738c1a007ed612bf195ad4ac61d234c8
VagrantPi <xup6u06m4@gmail.com>
2021-07-17 00:09:24 +0800

    finish update lending balance

--- app.js
+++ app.js
@@ -4,23 +4,37 @@ const HttpAgent = require('./lib/HttpAgent');
 const main = async () => {
   const config = await Utils.readConfig()
   const httpAgent = new HttpAgent()
-  const timestamp = new Date().getTime().toString()
 
-  const signature = Utils.sign({
-    apiMethod: 'GET',
-    apiPath: 'account', 
-    timestamp,
-    secret: config.ftx.apiSecret
-  })
-  console.log('signature', signature);
+  const intervalTime = 90000
 
-  const res = await httpAgent.get({
-    'FTX-KEY': config.ftx.apiKey,
-    'FTX-SIGN': signature,
-    'FTX-TS': timestamp,
-    'Content-Type': 'application/json; utf-8',
-  })
-  console.log('res', res);
+  setInterval(async () => {
+    const balancesResponse = await httpAgent.get({ path: 'wallet/balances', secret: config.ftx.apiSecret, apiKey: config.ftx.apiKey })
+
+    const updateCoins = ['BTC', 'DOGE', 'ASD', 'USD', 'USDT']
+
+    const coinTables = {}
+
+    if (!balancesResponse.success) {
+      console.log('get balance error');
+      return
+    }
+    for (let i = 0; i < balancesResponse.result.length; i++) {
+      const coinBalance = balancesResponse.result[i];
+      coinTables[coinBalance.coin] = coinBalance
+    }
+    
+    for (let i = 0; i < updateCoins.length; i++) {
+      const updateCoin = updateCoins[i];
+
+      const body = {
+        coin: updateCoin,
+        size: coinTables[updateCoin].total,
+        rate: 1e-6
+      }
+      const lendingResponse = await httpAgent.post({ path: 'spot_margin/offers', secret: config.ftx.apiSecret, apiKey: config.ftx.apiKey, body })
+      if (!lendingResponse.success) console.log(`update ${updateCoin} lending balance false!`);
+    }
+  }, intervalTime);
 }
 
 main()
