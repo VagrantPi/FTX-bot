b9e686494acc3acda624932d73ea982c6c655424
VagrantPi <xup6u06m4@gmail.com>
2021-07-25 20:01:32 +0800

    add daily total earn

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const Utils = require('./lib/Utils');
 const LoggerAdapter = require('./lib/LoggerAdapter');
 const FTXHTTPAgent = require('./lib/FTXHttpAgent');
 const DB = require('./db/model');
-const LendingBot = require('./lib/LendingBot');
 const WebServer = require('./lib/WebServer');
 
 const main = async () => {
@@ -13,16 +12,11 @@ const main = async () => {
   const httpAgent = new FTXHTTPAgent();
   const dbInstance = new DB({ dbConfig: config.database, logger });
   const db = await dbInstance.initialORM();
-  const lendingBot = new LendingBot({
-    config, logger, db, httpAgent, updateCoins,
-  });
 
   const webServer = new WebServer({
     config, logger, db, httpAgent, updateCoins,
   });
   logger.log('service start!!');
-
-  await lendingBot.start();
 
   await webServer.start();
 };
